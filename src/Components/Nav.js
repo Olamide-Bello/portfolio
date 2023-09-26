05561bd14b0a07ffbf4edfaa5482d15036d402c6
Olamide-Bello <belloolamideakinniyi@gmail.com>
2023-09-26 20:09:05 +0100

    added hamburger menu

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,22 +1,38 @@
 import moment from 'moment/moment'
-import React from 'react'
+import React, { useState } from 'react'
+import Hamburger from '../Assets/Hamburger.png'
+import Exit from '../Assets/Exit.png'
 
 const Nav = () => {
-  return (
-    <nav>
-        <div className='date'>
-            Today, {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
-
-        <ul>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#skills'>Skills</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#experience'>Experience</a></li>
-            <li><a href='#contact'>Contact</a></li>
-        </ul>
-    </nav>
-  )
+    const [menu, setMenu] = useState(false)
+    const handleMenu = () => {
+        setMenu(!menu)
+    }
+    return (
+        <nav>
+            <div className='date'>
+                Today, {moment().format('MMMM Do YYYY, h:mm:ss a')}
+            </div>
+            <ul>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#skills'>Skills</a></li>
+                <li><a href='#projects'>Projects</a></li>
+                <li><a href='#experience'>Experience</a></li>
+                <li><a href='#contact'>Contact</a></li>
+            </ul>
+            <img onClick={handleMenu} className='hamburger' src={Hamburger} alt='Hamburger menu'/>
+            {menu && <div className='menu'>
+                <img onClick={handleMenu} className='exit' src={Exit} alt='exit icon' />
+                <ul>
+                    <li><a href='#about'>About</a></li>
+                    <li><a href='#skills'>Skills</a></li>
+                    <li><a href='#projects'>Projects</a></li>
+                    <li><a href='#experience'>Experience</a></li>
+                    <li><a href='#contact'>Contact</a></li>
+                </ul>
+            </div>}
+        </nav>
+    )
 }
 
 export default Nav
